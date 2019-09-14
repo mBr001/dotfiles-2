9d9227069824c82551a2394a2ded420267e15a59
Srijon Saha <srijon365@gmail.com>
2019-09-14 15:53:15 -0700

    Add editor settings

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -sf "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sf "$DOTFILES_DIR/zsh/.zshrc" ~
+ln -sf "$DOTFILES_DIR/vscode/settings.json" ~/Library/Application\ Support/Code/User/settings.json
+ln -sf "$DOTFILES_DIR/sublime/Preferences.sublime-settings" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
